fa3bcf2e457d61c81cf361a0a8afe6c444345670
Mark Lee Chu Yong <markleechuyong@gmail.com>
2016-06-25 08:16:46 +0100

    Updated the requirements, extract out API key

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import json
+import requests
+
 app = Flask(__name__)
 
 
@@ -10,14 +12,63 @@ def hello():
 
 @app.route("/findroute")
 def find_route():
-    start = ["567987", "730157", "596195"]
-    destinations = [
+    start_list = ["567987", "730157", "596195", "348271"]
+    dest_list = [
         "Junction 8",
         "Plaza Singapura",
+        "Nex",
+        "JEM",
         "Star Vista",
-        "Causeway Point"
+        "Causeway Point",
+        "AMK Hub"
     ]
-    return json.dumps(start)
+
+    start = [Source(x) for x in start_list]
+    dest = [Destination(x) for x in dest_list]
+
+    for s in start:
+        for d in dest:
+            get_time(s, d)
+
+    for d in dest:
+        print d.location, d.time
+
+    return "hello"
+
+
+def get_time(start, dest):
+    de_req = "http://www.streetdirectory.com/api/?mode=search&act=all"
+    de_req += "&profile=sd_mobile&country=sg&q=" + dest.location
+    de_req += "&output=json&start=0&limit=1"
+
+    r = requests.get(de_req)
+    resp = json.loads(r.text)
+    dest.google_location = resp[1]['a']
+
+    api_key = "AIzaSyDwQagrpe0UCbH3s1p5eLDDCVLYvMJ59gc"
+    req = "https://maps.googleapis.com/maps/api/directions/json?"
+    req += "origin=" + start.location
+    req += "&destination=" + dest.google_location
+    req += "&region=sg&mode=transit&key=" + api_key
+
+    r = requests.get(req)
+    resp = json.loads(r.text)
+    time = resp['routes'][0]['legs'][0]['duration']['value']
+    dest.time += int(time) / 60
+    print start.location, dest.location, time / 60
+
+
+class Destination:
+    def __init__(self, loc):
+        self.location = loc
+        self.google_location = ""
+        self.time = 0
+
+
+class Source:
+    def __init__(self, loc):
+        self.location = loc
+
 
 if __name__ == "__main__":
     app.run()
